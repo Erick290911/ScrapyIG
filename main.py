5f6374f8fab0f4b77f3bf749a9b81fad41b9531b
Erick290911 <121366044+Erick290911@users.noreply.github.com>
2024-12-11 06:47:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI  # Importa la clase principal FastAPI
 
-app = FastAPI()
+app = FastAPI() # Crea una instancia de la aplicación FastAPI
 
 
-@app.get("/saludo/{frase}")
-def custom_saludo(frase: str):
+@app.get("/saludo/{frase}") # Define una ruta (endpoint) para manejar solicitudes GET en "/"
+def custom_saludo(frase: str): #Resive la frase desde el apartadourl 
     return {"message": frase}
